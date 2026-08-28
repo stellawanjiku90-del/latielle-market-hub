a6e9c198ca89fa6614bcb4dd3da9dc916ee47e24
stellawanjiku90-del <stellawanjiku90@gmail.com>
2026-08-28 17:47:30 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ProtectedRoute from '@/components/ProtectedRoute';
 import OfflineGate from '@/components/OfflineGate';
 import { AuthProvider } from '@/lib/AuthContext';
 import Layout from './components/Layout.jsx';
+import ScrollToTop from '@/components/ScrollToTop';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
@@ -68,6 +69,7 @@ function App() {
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
         <Router>
+          <ScrollToTop />
           <OfflineGate>
             <AuthenticatedApp />
           </OfflineGate>
